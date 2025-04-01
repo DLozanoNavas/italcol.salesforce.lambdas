6eaa6ba508c7b4677d331e427807cff5ae1cdad8
DAlejandroB <diegoaenciso76@gmail.com>
2025-04-01 17:18:53 -0500

    Implement SQL Server connection in Lambda function

--- lambda/lamda_function.py
+++ lambda/lamda_function.py
@@ -1,3 +1,33 @@
+import pymssql
+import os
+
 def lambda_handler(event, context):
-  print("Hello from test AWS Lambda!")
-  return { "statusCode":200, "body": "Hello from AWS lambda!"}
+    try:
+        # Datos de conexión
+        server = os.environ['DB_HOST']  # Dirección del RDS
+        user = os.environ['DB_USER']    # Usuario de la base de datos
+        password = os.environ['DB_PASS']  # Contraseña
+        database = os.environ['DB_NAME']  # Nombre de la base de datos
+
+        # Conectar a SQL Server
+        conn = pymssql.connect(server, user, password, database)
+        cursor = conn.cursor()
+        
+        # Consulta de prueba
+        cursor.execute("SELECT @@VERSION")
+        row = cursor.fetchone()
+
+        # Cerrar la conexión
+        cursor.close()
+        conn.close()
+
+        return {
+            "statusCode": 200,
+            "body": f"Conexión exitosa a SQL Server. Versión: {row[0]}"
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": f"Error en la conexión: {str(e)}"
+        }
