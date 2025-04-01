827f9015daca96ee99cdce805f922bb02adb5b88
DAlejandroB <diegoaenciso76@gmail.com>
2025-04-01 18:18:39 -0500

    Refactor environment configuration to use environment variables for account and region

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ import aws_cdk as cdk
 from italcol_salesforce_lambdas.italcol_salesforce_lambdas_stack import ItalcolSalesforceLambdasStack
 
 
+account = os.getenv("CDK_DEFAULT_ACCOUNT", os.getenv("AWS_ACCOUNT_ID"))
+region = os.getenv("CDK_DEFAULT_REGION", os.getenv("AWS_REGION"))
+
 app = cdk.App()
 ItalcolSalesforceLambdasStack(app, "ItalcolSalesforceLambdasStack",
 
     # Stack deployed with corresponding account and region configured
-    env=cdk.Environment(account=cdk.Aws.ACCOUNT_ID, region=cdk.Aws.REGION),
+    env=cdk.Environment(account=account, region=region),
     )
 
 app.synth()
