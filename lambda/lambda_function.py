d7bda768d824688cfaf25916ee1921a73ff267d4
DAlejandroB <diegoaenciso76@gmail.com>
2025-04-16 15:44:00 -0500

    Refactor lambda_handler to enhance configuration loading, database connection, and API authentication; improve data mapping and error handling

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,41 +3,49 @@ import os
 import requests
 import json
 
-# This is the lambda handler for the THIRD PARTIES view
+ALLOWED_COMPANIES = {"CASABLANCA", "ITALCOL", "SANTA_REYES"}
+
+# Lambda handler for the THIRD PARTIES view
 def lambda_handler(event, context):
     try:
         with open("config.json", "r") as file:
             config = json.load(file)
 
-        # Set configurations
-        VIEW_TYPE = "THIRD_PARTIES"
-        VIEWS = config["VIEWS"]
-        API_URIS = config["API_URIS"]
+        # Set configuration
         selected_company = event.get("selected_company")
+        validate_selected_company(selected_company)
 
         # Connect to database
         cursor, conn = connect_to_database(selected_company)
         
         # Select view and perform query
-        query = VIEWS[VIEW_TYPE]
+        query = config["VIEW"]
         cursor.execute(query)
-        
-        columns = [col[0] for col in cursor.description]
 
+        # Convert query results to usable data        
+        columns = [col[0] for col in cursor.description]
         rows = [dict(zip(columns,row)) for row in cursor.fetchall()]
-        
         mapped_data = map_from_view(selected_company, rows)
 
-        print(mapped_data)
-        #lambda_client = boto3.client('lambda')
-        #response = lambda_client.invoke(
-        #    FunctionName="Test_ConectVPC",
-        #    InvocationType="Event",
-        #    Payload=json.dumps(mapped_data, default=str)
-        #)
+        # Authenticate against salesforce API        
+        auth_params = config["AUTH_PARAMS"]         #NOTE this should be retrieved from SecretsManager when using prod credentials
+        auth_url = config["AUTH_URL"]
+        auth_response = authenticate(auth_url, auth_params)
 
-        #print(response)
+        # Send the mapped results from the Views to the respective salesforce endpoints
+        post_responses = []
+        for body in mapped_data:
+            try:
+                response = requests.post(
+                    url = f"{auth_response['base_url']}{config['API_URI']}",
+                    json = body,
+                    headers = {"Content-Type" : "application/json", "Authorization" : f"Bearer {auth_response['access_token']}"}
+                )
+                post_responses.append(response.json())
+            except Exception as e:
+                post_responses.append({"error":str(e)})
 
+        print(post_responses)
         # Close connection
         cursor.close()
         conn.close()
@@ -55,12 +63,12 @@ def lambda_handler(event, context):
     
 def map_from_view(selected_company, db_rows):
     # Mapping logic based on what view was selected
-    if selected_company == "CASABLANCA":
-        client_id_field_name = "Id_Cliente_ERP_Casablanca__c"
-    elif selected_company == "ITALCOL":
-        client_id_field_name = "Id_Cliente_ERP_Balanceados__c"
-    elif selected_company == "SANTA_REYES":
-        client_id_field_name = "Id_Cliente_ERP_Italhuevo__c"
+    COMPANY_ID_FIELD_MAP = {
+        "CASABLANCA": "Id_Cliente_ERP_Casablanca__c",
+        "ITALCOL": "Id_Cliente_ERP_Balanceados__c",
+        "SANTA_REYES": "Id_Cliente_ERP_Italhuevo__c"
+    }
+    client_id_field_name = COMPANY_ID_FIELD_MAP.get(selected_company)
         
     mapped_list = []
 
@@ -68,7 +76,7 @@ def map_from_view(selected_company, db_rows):
         row_body = {
             client_id_field_name : row["F200_nit"],
             "AccountNumber" : row["F200_nit"],
-            "Type" : "Costumer",
+            "Type" : "Customer",
             "Industry" : "Retail",        
             "ShippingCity": row["f011_descripcion"],
             "ShippingCountry": row["f013_descripcion"], 
@@ -80,8 +88,8 @@ def map_from_view(selected_company, db_rows):
             row_body["Name"] = row["f200_razon_social"]
             row_body["cu_colaborador_grupo_italcol__c"] = False
         elif row["f200_id_tipo_ident"] == "C" or row["f200_id_tipo_ident"] == "E":
-            row_body["FirstName"] = row[""]
-            row_body["LastName"] = f"{row["f200_apellido1"]} {row["f200_apellido2"]}"
+            row_body["FirstName"] = row["f200_nombres"]
+            row_body["LastName"] = f"{row['f200_apellido1']} {row['f200_apellido2']}"
             row_body["Salutation"] = "Sr."
             row_body["cu_colaborador_grupo_italcol__c"] = True
             # Si es colaborador se envia campo co_compania
@@ -109,3 +117,21 @@ def connect_to_database(selected_company):
     conn = pymssql.connect(server, user, password, database)
     cursor = conn.cursor()
     return cursor, conn
+
+def authenticate(url, params):
+  response = requests.post(url=url, params=params, headers={"Content-Type": "application/json"})
+
+  if response.status_code == 200:
+    token_data = response.json()
+    return {
+      "access_token" : token_data.get("access_token"),
+      "base_url" : token_data.get("instance_url")
+    }
+  raise Exception(f"Authentication failed: {response.text}")
+
+def validate_selected_company(selected_company):
+    if not selected_company:
+        raise ValueError("Missing 'selected_company' in request.")
+
+    if selected_company not in ALLOWED_COMPANIES:
+        raise ValueError(f"Invalid 'selected_company': '{selected_company}'. Must be one of {', '.join(ALLOWED_COMPANIES)}.")
